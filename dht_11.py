e2e0f0d206b3207524812b96c3182e9f3daaa9f5
Tony-UWR <thomasantony.uwr@gmail.com>
2024-09-23 17:25:47 +0530

    Update dht_11.py

--- dht_11.py
+++ dht_11.py
@@ -1,19 +1,37 @@
-import Adafruit_DHT
-import time
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-# Set the sensor type and the GPIO pin
-sensor = Adafruit_DHT.DHT11
-pin = 4  # GPIO pin number where the data pin of DHT11 is connected
+import time
+import board
+import adafruit_dht
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D2)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
-    # Read the humidity and temperature from the sensor
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
 
-    # Check if the reading was successful
-    if humidity is not None and temperature is not None:
-        print(f'Temperature: {temperature}°C  Humidity: {humidity}%')
-    else:
-        print('Failed to get reading. Try again!')
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
-    # Wait for a while before the next reading
-    time.sleep(2)
+    time.sleep(2.0)
