ff514b2b15748d50378e0f7b9523d0424c12d26b
Tony-UWR <thomasantony.uwr@gmail.com>
2024-11-07 17:23:58 +0530

    Create IoT.py

---
+++ IoT.py
@@ -0,0 +1,45 @@
+import BlynkLib
+import RPi.GPIO as GPIO
+from BlynkTimer import BlynkTimer
+
+BLYNK_AUTH_TOKEN = 'b01O8czP4P6PadYNeWh5DFqmH1CbKCZO'
+
+led1 = 14
+led2 = 15
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+
+x = 20
+# Initialize Blynk
+blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN)
+
+# Led control through V0 virtual pin
+@blynk.on("V0")
+def v0_write_handler(value):
+#    global led_switch
+    if int(value[0]) is not 0:
+        GPIO.output(led1, GPIO.HIGH)
+        print('LED1 HIGH')
+    else:
+        GPIO.output(led1, GPIO.LOW)
+        print('LED1 LOW')
+
+# Led control through V0 virtual pin
+@blynk.on("V1")
+def v1_write_handler(value):
+#    global led_switch
+    if int(value[0]) is not 0:
+        GPIO.output(led2, GPIO.HIGH)
+        print('LED2 HIGH')
+    else:
+        GPIO.output(led2, GPIO.LOW)
+        print('LED2 LOW')
+
+#function to sync the data from virtual pins
+@blynk.on("connected")
+def blynk_connected():
+    print("Raspberry Pi Connected to New Blynk") 
+
+while True:
+    blynk.run()
